125f4a1f43ab9e1b776c1b187e8a4572290d3d29
Charles Kuo <CharlesK@ashn.com>
2018-05-15 14:33:28 -0700

    Ch 2 "Getting up to speed" completed

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,8 +1,9 @@
 // in app/components/pac-man.js
 import Ember from 'ember';
 import Component from '@ember/component';
-
-export default Component.extend({
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
+// component can be js and / or handlebars 
+export default Component.extend(KeyboardShortcuts, {
   // need html already been rendered
   // didInsertElement runs whenever the component is loaded and put on screen 
   didInsertElement: function() {
@@ -21,6 +22,13 @@ export default Component.extend({
     ctx.arc(x, y, radius, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
+  },
+
+  keyboardShortcuts: {
+    up: function() { console.log('up');},
+    down: function() { console.log('down');},
+    left: function() { console.log('left');},
+    right: function() { console.log('right');},
   },
 });
 
