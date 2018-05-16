5b795bfb003ff0d7e529566c9461df8c48df3df4
Charles Kuo <CharlesK@ashn.com>
2018-05-15 17:40:03 -0700

    Ch 4 "Movement" completed

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -10,12 +10,21 @@ export default Component.extend(KeyboardShortcuts, {
     this.drawCircle();
   },
 
-  drawCircle: function() {
+  x: 50,
+  y: 100,
+  squareSize: 40,
+
+  ctx: Ember.computed(function() {
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
-    let x = 50;
-    let y = 100;
-    let radius = 20;
+    return ctx;
+  }),
+
+  drawCircle: function() {
+    let ctx = this.get('ctx');
+    let x = this.get('x');
+    let y = this.get('y');
+    let radius = this.get('squareSize') / 2;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
@@ -25,11 +34,33 @@ export default Component.extend(KeyboardShortcuts, {
   },
 
   keyboardShortcuts: {
-    up: function() { console.log('up');},
-    down: function() { console.log('down');},
-    left: function() { console.log('left');},
-    right: function() { console.log('right');},
+    up: function() { 
+      this.movePacMan('y', -1 * this.get('squareSize'))
+    },
+    down: function() { 
+      this.movePacMan('y', this.get('squareSize'));
+    },
+    left: function() { 
+      this.movePacMan('x', -1 * this.get('squareSize'));
+    },
+    right: function() { 
+      this.movePacMan('x', this.get('squareSize'));
+    },
   },
+
+  clearScreen: function() {
+    let ctx = this.get('ctx');
+    let screenWidth = 800;
+    let screenHeight = 600;
+
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
+  },
+
+  movePacMan: function(direction, amount) {
+    this.incrementProperty(direction, amount);
+    this.clearScreen();
+    this.drawCircle();
+  }
 });
 
 
